20e9d485c5fde1e5fe0a2986b60b9750b12888aa
root <admin@example.com>
2017-01-01 19:24:47 -0500

    Server is returning an object to webpage

--- app.js
+++ app.js
@@ -25,122 +25,77 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
 
-// var imageList = {};
-// function readImageFiles(dirname) {
-//     fs.readdir(dirname, function (dirname, filenames) {
-//         // if(err){
-//         //     onError(err);
-//         //     return;
-//         // }
-//         filenames.forEach(function (filename) {
-//           console.log(filename);
-//             // imageList[i] = filename;
-//         })
+
+// app.get('/images', function (req, res) {
+//     // res.send('SUCESS');
+//     console.log('starting server process');
+//
+//     //This gives a path to the directory on server
+//     // 'fs.readdir' process the directory
+//     var imgPath = path.join(__dirname + '/public/images');
+//     fs.readdir(imgPath, function (err, files) {
+//         files.forEach(function (file) {
+//             if (file.indexOf('.jpg') != -1) {
+//                 console.log(file);
+//             }
+//         });
 //     });
-// }
-
-app.get('/images', function (req, res) {
-    // res.send('SUCESS');
-    console.log('starting server process');
-    fs.readdir(path.join(__dirname + '/public/images').toString(), function(err, files) {
-        files.forEach(function (file) {
-            console.log(file);
-            // if (file.indexOf('.jpg') != -1) {
-            //     console.log(file);
-            // }
-        });
-    });
-
-
-    // fs.pathname('/images').forEach(function (i,filename) {
-    //     console.log(filename)
-    // });
-
-    // var normalizedPath = require("path").join(__dirname, "routes");
-
-
-    // fs.readdirSync(normalizedPath).forEach(function(file) {
-    //     require("./routes/" + file);
-    // });
-
-
-
-});
+// });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
+// var imgData = [];
+// var imgPath = path.join(__dirname + '/public/images');
 // Chat application for socket.io
 sockIO.on('connection', function (socket) {
-  console.log('A client connection occured!!!');
-  socket.on('send message', function(data){
-    console.log('Message recieved by server');
-    sockIO.emit('new message', data);
-    console.log('Message sent by server');
+    console.log('A client connection occured!!!');
+
+    // Send the chat data to server
+    socket.on('send message', function (data) {
+        sockIO.emit('new message', data);
     });
+
+// Create local variable with all images
+    socket.on('request-images', function() {
+        var imgData = [];
+        var imgPath = path.join(__dirname + '/public/images');
+
+        // Parse Directory
+        fs.readdir(imgPath, function (err, files) {
+            files.forEach(function (fileName) {
+                if (fileName.indexOf('.jpg') != -1) {
+                    imgData.push(fileName);
+                }
+            });
+            sockIO.emit('image-list', imgData);
+        });
+
+    });
+
 });
-
-
-
-// function request(request, response) {
-//     var store = '';
-//
-//     request.on('data', function(data)
-//     {
-//         store += data;
-//     });
-//     request.on('end', function()
-//     {  console.log(store);
-//         response.setHeader("Content-Type", "text/json");
-//         response.setHeader("Access-Control-Allow-Origin", "*");
-//         response.end(store)
-//     });
-// }
-
-
-
-// // Sort through image folder
-// function readImageFiles(dirname, onFileContent, onError) {
-//   fs.readdir(dirname, function(err, filenames){
-//     if(err){
-//       onError(err);
-//       return;
-//       }
-//       filenames.forEach(function (filename) {
-//         fs.readFile(dirname + filename, 'utf-8', function(err, content){
-//           if(err){
-//             onError(err);
-//             return;
-//           }
-//           onFileContent(filename,content);
-//         })
-//
-//       });
-//
-//   });
-//
 
 
 module.exports = app;
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
     // Create the socket.io connection
     var $socket = io.connect();
 
@@ -12,14 +12,13 @@ $(function(){
 
     // Add function when button clicked
     // Will submit text box
-    $toolbarButton.click(function(){
+    $toolbarButton.click(function () {
         $socket.emit('send message', $toolbarText.val());
         $toolbarText.val('');
-
     });
 
     // This message will recieve "data" as an object
-    $socket.on('new message', function(data){
+    $socket.on('new message', function (data) {
         console.log('Success Client Recieved!!!!');
         $displayText.append(data + "<br/>");
     });
@@ -32,45 +31,15 @@ $(function(){
 
     console.log('load ajax function');
 
+    $socket.emit('request-images');
 
-    $.ajax({
-        // type: 'GET',
-        url: '/images',
-        contentType: 'application/json',
-        success: function (data) {
-            console.log('Success!! Got an AJAX responce');
-            console.log(data);
-        }
+
+    $socket.on('image file path', function () {
+        console.log("success, I recieved the correct message");
     });
 
-    // function processImage(imgFile){
-    //     // if(!imgFile.contains($imgType))
-    //     //     return;
-    //
-    //     // process image
-    //     console.log('loading images');
-    //     $imageZone.append("<img src = " + $imgFolder + imgFile);
-    // }
-
-    // $.ajax({
-    //     url: $imgFolder,
-    //     success: function (data) {
-    //         $data.find(RegExp(".jpg")).each(function(img) {
-    //             console.log(imag);
-    //         });
-    //     }
-    // });
-    // $.ajax({
-    //     url: "/images/",
-    //     success: function(data){
-    //         $(data).find("a:contains(.jpg)").each(function(){
-    //             // will loop through
-    //             var images = $(this).attr("href");
-    //             console.log(images);
-    //             // $('<p></p>').html(images).appendTo('a div of your choice')
-    //
-    //         });
-    //     }
-    // });
-
+    $socket.on('image-list', function (data) {
+        console.log('recieving data');
+        console.log(data);
+    });
 });
